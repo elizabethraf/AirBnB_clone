449adf12d465e12d622442e69c35ab06c85fedf5
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-10-29 13:50:00 -0700

    engine

---
+++ models/engine/file_storage.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Display FileStorage that serializes instances to a JSON file and deserializes
+JSON file to instances.
+"""
+
+from models.engine.file_storage import FileStorage
+import json
+
+
+class FileStorage:
+    """
+    serialisation instances to a JSON file and deserializes JSON file to instances
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """return the dictionary objects"""
+        return (self.__objects)
+    def new(self, obj):
+        """Display objects in sets"""
+        if obj:
+            self._objects["{}".format(str(type(obj).__name__), obj.id)] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file"""
+        self.__objects = __file_path()
+
+    def reload(self):
+        """deserializes the JSON file to __objects"""
+         if path.exists(self.__file_path):
+            with open(self.__file_path, mode='r', encoding='utf-8') as f:
+                json_dict = json.loads(f.read())
+                for k, v in json_dict.items():
+                    self.__objects[k] = eval(v['__class__'])(**v)
