8e39747403939fc8c8ec40e489ee64b95420530e
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-10-29 11:44:55 -0700

    test_base

--- models/base_model.py
+++ models/base_model.py
@@ -19,10 +19,17 @@ class BaseModel:
         """
         Initialising a method with it's  public instance attributes
         """
+         if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+    else:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-
+        models.storage.new(self)
     def __str__(self):
         """Display Printing Method"""
         return "[{}] ({}) {}".format(type(self).__name__, self.id,
