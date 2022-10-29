7fcf605dc4131e5597f2c7550d13783a83b79454
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-10-29 12:06:37 -0700

    recommit

--- models/base_model.py
+++ models/base_model.py
@@ -20,9 +20,16 @@ class BaseModel:
         Initialising a method with it's  public instance attributes
         """
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Display Printing Method"""
