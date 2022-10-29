35647b49c36d726737c7252ad351cc94cccf443d
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-10-29 11:01:57 -0700

    editing_base

--- models/base_model.py
+++ models/base_model.py
@@ -15,10 +15,19 @@ class BaseModel:
 
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initialising a method with it's  public instance attributes
         """
+
+         if kwargs:
+            for arg, val in kwargs.items():
+                if arg in ('created_at', 'updated_at'):
+                    val = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
+
+                if arg != '__class__':
+                    setattr(self, arg, val)
+        else:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
