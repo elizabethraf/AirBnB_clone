5da24bd008ad4fffa8b56f6496f951d5d5e1e50f
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-10-30 14:58:05 -0700

    python-iyoo

--- models/user.py
+++ models/user.py
@@ -7,10 +7,19 @@ from models.base_model import BaseModel, Base
 
 class User(BaseModel, Base):
     """Representation of a class for user """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    elif:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
