8ca33d1fcaeebc508495b01d3118d1ad7d849a57
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-10-30 07:47:08 -0700

    console.py

--- console.py
+++ console.py
@@ -10,22 +10,34 @@ class HBNBCommand(cmd.Cmd):
     """Dislplay  command interpreter"""
 
     prompt = '(hbnb) '
-
-    def update(self, arg):
-        """Update cmd"""
-        self.close()
-
-    def close(self):
-        if self.file:
-            self.file.EOF()
-            self.file = None
+    file = None
 
     def do_quit(self, arg):
-        """quit and EOF to exit the program"""
-        print('quit')
+        """Quit command to exit the program"""
         self.close()
         quit()
         return True
+
+    def do_EOF(self, line):
+        """Quit command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """Called when an empty line is entered in response to the prompt.
+
+        If this method is not overridden, it repeats the last nonempty
+        command entered.
+
+        """
+        if self.lastcmd:
+            self.lastcmd = ""
+            return self.onecmd('\n')
+
+    def close(self):
+        if self.file:
+            self.file.close()
+            self.file = None
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
