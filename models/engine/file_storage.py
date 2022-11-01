eb5ea5e0bef50849ee8c9570478b6cfe1d6d847a
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-11-01 14:28:35 -0700

    fix pycodestype storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -37,7 +37,8 @@ class FileStorage:
     def reload(self):
         """deserializes the JSON file to __objects"""
         if os.path.exists(FileStorage.__file_path):
-            with open(FileStorage.__file_path, mode='r', encoding='utf-8') as f:
+            with open(FileStorage.__file_path, mode='r',
+                      encoding='utf-8') as f:
                 json_dict = json.loads(f.read())
                 for a, b in json_dict.items():
                     FileStorage.__objects[a] = eval(b['__class__'])(**b)
